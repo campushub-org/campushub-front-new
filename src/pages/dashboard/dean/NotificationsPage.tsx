ecad0aa7a2d045849ecc7dae823a546204d146a9
JordanFotso <achillefotso43@gmail.com>
2026-03-28 20:12:27 +0100

    feat:dean notification upgrade

--- src/pages/dashboard/dean/NotificationsPage.tsx
+++ src/pages/dashboard/dean/NotificationsPage.tsx
@@ -71,6 +71,13 @@ const DeanNotificationsPage: React.FC = () => {
 
   useEffect(() => {
     fetchNotifications();
+
+    const handleNewNotif = () => {
+      fetchNotifications();
+    };
+
+    window.addEventListener('notification_received', handleNewNotif);
+    return () => window.removeEventListener('notification_received', handleNewNotif);
   }, []);
 
   const handleMarkAsRead = async (userNotificationId: number) => {
